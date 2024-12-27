15a3472229cd04eb135e9e6a6bc20e2ec80cd0ad
romariodomingos <romaogando@gmail.com>
2024-12-27 01:16:00 +0100

    Change Blue

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function generateQRCode() {
     const data = document.getElementById("data").value.trim();
     const qrCodeContainer = document.getElementById("qrcode");
-    const downloadLink = document.getElementById("download");
+    const imgCode = document.getElementById("imgcode");
 
     if (data === "") {
       alert("Por favor, insira algum texto para gerar o QR Code.");
@@ -18,21 +18,14 @@ function generateQRCode() {
       correctLevel: QRCode.CorrectLevel.H
     });
 
-    // Aguarda o QR Code ser renderizado antes de gerar o link de download
-    setTimeout(() => {
-      // Localiza o elemento <canvas> dentro do contêiner
-      const canvas = qrCodeContainer.querySelector("canvas");
-      if (canvas) {
-        // Ajustando a imagem gerada para o tamanho e resolução desejada
-        const dataURL = canvas.toDataURL("image/png"); // Converte para URL da imagem
-        downloadLink.href = dataURL; // Define o href para o Data URL
-        downloadLink.style.display = "inline-block"; // Mostra o botão de download
-      }
-    }, 100); // Pequeno atraso para garantir que o QR Code foi renderizado
 
     // Exibe o texto indicando sucesso
     document.getElementById("textCode").style.display = "block";
-  }
+
+     // O cultar a img indicando sucesso
+     document.getElementById("imgCode").style.display = "none";
+    
+}
 
   // Adiciona o evento de clique ao botão
   document.getElementById("botao").addEventListener("click", generateQRCode);
